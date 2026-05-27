1b69655fe9a02ebdf1f02451faa45f1a214f78a1
chibunduekwunife <chibunduekwunife@gmail.com>
2026-05-27 13:59:52 -0400

    grab input values and log to console

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function generateReply(){
+    const emailInput = document.getElementById("emailInput").value;
+    const toneSelect = document.getElementById("toneSelect").value;
+
+    console.log(emailInput, toneSelect)
+}
