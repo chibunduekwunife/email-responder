46b355a139a26bd13b4f41b3ba97fbf27550d0a1
chibunduekwunife <chibunduekwunife@gmail.com>
2026-05-28 14:47:41 -0400

    add loading state to generate button

--- script.js
+++ script.js
@@ -2,11 +2,15 @@
 async function generateReply(){
     const email = document.getElementById("emailInput").value;
     const tone = document.getElementById("toneSelect").value;
+    const button = document.getElementById("button");
 
     if (email == ""){
         document.getElementById("output").textContent = "Make sure to type in the email before clicking generate"
         return
     }
+
+    button.disabled = true;
+    button.textContent = "Generating...";
 
     try{
         const response  = await fetch(
@@ -29,6 +33,9 @@ async function generateReply(){
     } catch (error) {
         document.getElementById("output").textContent = "server error"
     }
+
+    button.disabled = false;
+    button.textContent = "Generate reply";
     
 
 }
