db12d854836b604f0899a72f051721979978dd8b
chibunduekwunife <chibunduekwunife@gmail.com>
2026-05-28 12:03:02 -0400

    wire up frontend to server - working end to end

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 async function generateReply(){
-    const emailInput = document.getElementById("emailInput").value;
-    const toneSelect = document.getElementById("toneSelect").value;
+    const email = document.getElementById("emailInput").value;
+    const tone = document.getElementById("toneSelect").value;
 
     const response  = await fetch(
-        "https://api.anthropic.com/v1/messages", {
+        "http://localhost:3000/generate", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                model: "claude-sonnet-4-20250514",
-                max_tokens: 1000,
-                messages: [{
-                    role: "user",
-                    content: `Write a reply to this email in a ${toneSelect} tone: ${emailInput}`
-                }]
-
+                email,
+                tone
             })
         }
     )
 
     const data = await response.json()
-    console.log(data)
+    const subject = data.content[0].text
+
+    document.getElementById("output").textContent = subject
 }
